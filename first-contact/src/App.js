45611174810ee1eb579872f5a81e94b7c0f169c6
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-03-06 23:40:25 -0300

    added a button

--- first-contact/src/App.js
+++ first-contact/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <button type="button">Mostrar</button>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
