5e02b268a17691d3d54712a8c94f0cf9097e0e4f
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-03-06 23:37:26 -0300

    Update index.js

--- first-contact/src/index.js
+++ first-contact/src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class App extends Component {
+
+
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
