a99855a394a4a0547882c812f72a8411b82cf41a
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-03-06 23:51:52 -0300

    Update App.js

--- first-contact/src/App.js
+++ first-contact/src/App.js
@@ -23,7 +23,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-        <button type="button" onClick={this.toggle}>Mostrar</button>
+        <button type="button" onClick={this.toggle}>
+          Mostrar
+        </button>
           {this.state.showImage && (
             <img src={logo} className="App-logo" alt="logo" />
           )}
